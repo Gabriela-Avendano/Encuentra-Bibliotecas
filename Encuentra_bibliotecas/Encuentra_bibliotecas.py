d7ffccc316ca97f4d54d96b69135ae19944d1258
Jaimit00708 <jaime.gonzalez.pin@usach.cl>
2025-01-02 03:20:59 -0300

    Update Encuentra_bibliotecas.py

--- Encuentra_bibliotecas/Encuentra_bibliotecas.py
+++ Encuentra_bibliotecas/Encuentra_bibliotecas.py
@@ -16,36 +16,63 @@ class TextAreaFeedbackState(rx.State):
                     #self.attach_screenshot = True
                     self.submitted = False
 
-def feedback_form():
-                return rx.cond(
-                    TextAreaFeedbackState.submitted,
-                    rx.card(
-                        rx.vstack(
-                            rx.text("¡Gracias por tu aporte!"),
-                            rx.button(
-                                "¿Tienes algo más que avisar?",
-                                on_click=TextAreaFeedbackState.reset_form,
-                            ),
-                        ),
+class DynamicFormState(rx.State):
+    form_data: dict = {}
+    form_fields: list[str] = [
+        "Nombre",
+        "Apellido",
+        "correo",
+        "Comentario"
+
+    ]
+
+    @rx.var(cache=True)
+    def form_field_placeholders(self) -> list[str]:
+        return [
+            " ".join(
+                w.capitalize() for w in field.split("_")
+            )
+            for field in self.form_fields
+        ]
+
+    @rx.event
+    def add_field(self, form_data: dict):
+        new_field = form_data.get("new_field")
+        if not new_field:
+            return
+        field_name = (
+            new_field.strip().lower().replace(" ", "_")
+        )
+        self.form_fields.append(field_name)
+
+    @rx.event
+    def handle_submit(self, form_data: dict):
+        self.form_data = form_data
+
+
+def dynamic_form():
+    return rx.vstack(
+        rx.form(
+            rx.vstack(
+                rx.foreach(
+                    DynamicFormState.form_fields,
+                    lambda field, idx: rx.input(
+                        placeholder=DynamicFormState.form_field_placeholders[
+                            idx
+                        ],
+                        name=field,
                     ),
-                    rx.card(
-                        rx.form(
-                            rx.flex(
-                                rx.text("¿Tienes algún aviso por hacer?"),
-                                rx.text_area(
-                                    placeholder="Escribe aquí...",
-                                    value=TextAreaFeedbackState.feedback,
-                                    on_change=TextAreaFeedbackState.set_feedback,
-                                    resize="vertical",
-                                ),
-                                rx.button("Enviar", type="submit"),
-                                direction="column",
-                                spacing="3",
-                            ),
-                            on_submit=TextAreaFeedbackState.submit_feedback,
-                        ),
-                    ),
-                )
+                ),
+                rx.button("Enviar", type="submit"),
+            ),
+            on_submit=DynamicFormState.handle_submit,
+            reset_on_submit=True,
+        ),
+        
+        rx.divider(),
+        rx.heading("Último comentario", color= "Purple"),
+        rx.text(DynamicFormState.form_data.to_string()),
+    )
 #Definimos la pagina principal
 def index() -> rx.Component:
     return rx.container(
@@ -59,29 +86,9 @@ def index() -> rx.Component:
             
             rx.text('Aqui encontrarás información acerca de la disponibilidad de las bibliotecas que frecuentas dentro de la universidad', style={'color': 'Green','font_size': '2.5em'}),
 
-            rx.text("Listado de bibliotecas:\n", style={'color': 'Orange'}),
-            
-            #rx.list(
-                #rx.list.item('Biblioteca Central'),
-                #rx.list.item('Facultad de Administración y Economía'),
-                #rx.list.item('Facultad de Arquitectura y Ambiente Construido'),
-                #rx.list.item('Facultad de Ciencias Médicas'),
-                #rx.list.item('Facultad de Derecho'),
-                #rx.list.item('Facultad de Humanidades'),
-                #rx.list.item('Facultad de Química y Biología'),
-                #rx.list.item('Departamento de Física'),
-                #rx.list.item('Departamento de Ingeniería Eléctrica'),
-                #rx.list.item('Departamento de Ingeniería GeoEspacial y Ambiental'),
-                #rx.list.item('Departamento de Ingeniería Industrial'),
-                #rx.list.item('Departamento de Ingeniería Informática'),
-                #rx.list.item('Departamento de Ingeniería Mecánica'),
-                #rx.list.item('Departamento de Ingeniería en Minas'),
-                #rx.list.item('Departamento de Matemáticas y Ciencias de la Computación'),
-                #rx.list.item('Escuela de Periodismo'),
-                #rx.list.item('Facultad Tecnológica'),
-                #rx.list.item('Instituto de Estudios Avanzados'),
+            rx.text("Listado de bibliotecas:\n", style={'color': 'Orange','font_size': '2em'}),
+    
 
-            #),
             rx.link("Biblioteca Central", href="/biblioteca_central", style={"color": "sky blue", "text-decoration": "underline"}),
             rx.link("Facultad de Administración y Economía", href="/fae", style={"color": "sky blue", "text-decoration": "underline"}),
             rx.link("Facultad de Arquitectura y Ambiente Construido", href="/ARQUITECTURA", style={"color": "sky blue", "text-decoration": "underline"}),
@@ -98,8 +105,7 @@ def index() -> rx.Component:
             rx.link("Departamento de Ingeniería en Minas", href="/D_MINAS", style={"color": "sky blue", "text-decoration": "underline"}),
             rx.link("Departamento de Matemáticas y Ciencias de la Computación", href="/D_MATEMATICAS", style={"color": "sky blue", "text-decoration": "underline"}),
             rx.link("Escuela de Periodismo", href="/PERIODISMO", style={"color": "sky blue", "text-decoration": "underline"}),
-            rx.link("Facultad Tecnológica", href="/TECNOLOGICA", style={"color": "sky blue", "text-decoration": "underline"}),
-            rx.link("Instituto de Estudios Avanzados", href="/ESTUDIOS_AVANZADOS", style={"color": "sky blue", "text-decoration": "underline"}),
+            rx.link("Facultad Tecnológica", href="/TECNOLOGICA", style={"color": "sky blue", "text-decoration": "underline"})
 
         
             
@@ -112,21 +118,15 @@ def index() -> rx.Component:
 def pagina_nueva() -> rx.Component:
     return rx.container(
         rx.vstack(
-            rx.text('Biblioteca', front_size = "2em", front_weight="bold"),
-            feedback_form(),
-            rx.text('Avisos'),
+            rx.text('Biblioteca', font_size = "2em", font_weight="bold", color="yellow"),
+            #feedback_form(),
+            rx.text('Avisos', font_size= '1.5em', font_weight='bold', color='yellow'),
+            dynamic_form(),
+
             rx.link("Volver a la página principal", href="/", style={"color": "sky blue", "text-decoration": "underline"}),
         
         )
     )
-
-
-
-
-
-
-
-
 
 app = rx.App()
 app.add_page(index)
@@ -147,8 +147,3 @@ app.add_page(pagina_nueva, route="/D_MINAS")
 app.add_page(pagina_nueva, route="/D_MATEMATICAS")
 app.add_page(pagina_nueva, route="/PERIODISMO")
 app.add_page(pagina_nueva, route="/TECNOLOGICA")
-app.add_page(pagina_nueva, route="/ESTUDIOS_AVANZADOS")
-
-
-
-
